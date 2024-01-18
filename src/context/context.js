a17af69cff847629e21940e99a451ed9ada284b5
Manuel Rodriguez <greluma2@gmail.com>
2024-01-18 16:25:05 +0100

    search functionality

--- src/context/context.js
+++ src/context/context.js
@@ -6,6 +6,8 @@ import axios from "axios";
 import { PropTypes } from "prop-types";
 
 const rootUrl = "https://api.github.com";
+const searchUserUrl = "https://api.github.com/users/";
+// wesbos
 
 const GithubContext = React.createContext();
 
@@ -13,9 +15,61 @@ const GithubProvider = ({ children }) => {
   const [gitHubUser, setGitHubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [loading, seIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const searchGitHubUser = async (user) => {
+    toggleError();
+    // setLoading(true)
+    try {
+      const response = await axios(`${searchUserUrl}${user}`);
+      const userData = response.data;
+      console.log(userData);
+      setGitHubUser(userData);
+    } catch (error) {
+      console.log(error);
+      toggleError(true, "there is no user with that username");
+    }
+  };
+
+  // check rate
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "sorry, you have exceeded your hourly rate limit!");
+          console.log(error);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // error
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  useEffect(checkRequest, []);
 
   return (
-    <GithubContext.Provider value={{ gitHubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        gitHubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGitHubUser,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
